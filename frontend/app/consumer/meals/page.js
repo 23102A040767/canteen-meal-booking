9c5da14e99e97f05eb1aca6e43507f4142b738d8
Manaswi Boligarla <manaswiboligarla@gmail.com>
2026-06-27 23:25:22 +0530

    Added consumer order button navigation

--- frontend/app/consumer/meals/page.js
+++ frontend/app/consumer/meals/page.js
@@ -1,9 +1,12 @@
  "use client";
 
 import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function MealsPage() {
+
   const [meals, setMeals] = useState([]);
+  const router = useRouter();
 
   useEffect(() => {
     fetch("https://canteen-meal-booking-backend.onrender.com/meals/all")
@@ -14,17 +17,23 @@ export default function MealsPage() {
 
   return (
     <div className="min-h-screen bg-gray-100 p-8">
+
       <h1 className="text-4xl font-bold text-center mb-8">
         Available Meals
       </h1>
 
       <div className="grid gap-6">
+
         {meals.map((meal) => (
+
           <div
             key={meal.id}
             className="bg-white shadow-lg rounded-lg p-6"
           >
-            <h2 className="text-2xl font-bold">{meal.name}</h2>
+
+            <h2 className="text-2xl font-bold">
+              {meal.name}
+            </h2>
 
             <p className="text-lg mt-2">
               Price : ₹{meal.price}
@@ -35,13 +44,21 @@ export default function MealsPage() {
             </p>
 
             <button
+              onClick={() => {
+                localStorage.setItem("mealId", meal.id);
+                router.push("/consumer/order");
+              }}
               className="mt-4 bg-blue-600 text-white px-5 py-2 rounded hover:bg-blue-700"
             >
               Order
             </button>
+
           </div>
+
         ))}
+
       </div>
+
     </div>
   );
 }
