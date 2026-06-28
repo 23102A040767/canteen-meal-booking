253c546dd24d5de6bc75e3dbd12116d48dcdd8ee
Manaswi Boligarla <manaswiboligarla@gmail.com>
2026-06-28 09:03:54 +0530

    Improve order page UI

--- frontend/app/consumer/order/page.js
+++ frontend/app/consumer/order/page.js
@@ -58,7 +58,7 @@ export default function OrderPage() {
         localStorage.removeItem("mealId");
 
         setTimeout(() => {
-          router.push("/consumer/myorders");
+          router.push("/consumer/my-orders");
         }, 1500);
 
       } else {
